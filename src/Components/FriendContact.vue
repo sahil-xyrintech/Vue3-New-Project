ac8b79cca51185d21f2359fb19e740047948db3e
Sahil <sahil@xyrintech.com>
2023-01-29 23:08:35 +0530

    Code optimization.

--- src/Components/FriendContact.vue
+++ src/Components/FriendContact.vue
@@ -3,7 +3,7 @@
     class="border rounded-xl mx-auto inline-block p-6 bg-white/30 min-w-[300px]"
   >
     <h2 @click="toggleTrueFalse">
-      {{ name }} ({{ checkTrueFalse === "1" ? "True" : "False" }})
+      {{ name }} ({{ checkTrueFalse ? "True" : "False" }})
     </h2>
     <button
       class="border py-2 px-4 rounded-md bg-sky-500"
@@ -19,17 +19,16 @@
 </template>
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress", "trueFalse"],
+  props: {
+    name: String,
+    phoneNumber: String,
+    emailAddress: String,
+    trueFalse: String,
+  },
   data() {
     return {
       detailsAreVisible: false,
       checkTrueFalse: this.trueFalse,
-      friend: {
-        id: "manish",
-        name: "Manish Suthar",
-        phone: "9999999999",
-        email: "manish@gmail.com",
-      },
     };
   },
   methods: {
@@ -37,11 +36,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleTrueFalse() {
-      if (this.checkTrueFalse === "1") {
-        this.checkTrueFalse = "0";
-      } else {
-        this.checkTrueFalse = "1";
-      }
+      this.checkTrueFalse = !this.checkTrueFalse;
     },
   },
 };
